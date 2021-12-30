812cfaf9e30783d8212ca226a710a9d7550ffe6d
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2021-12-30 13:53:15 -0500

    Initial commit and has app icon and sample text for clock and date.

---
+++ App.tsx
@@ -0,0 +1,88 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+      <View style={styles.container}>
+        <Text style={styles.timeText} >12:00:00 PM</Text>
+        <Text style={styles.dateText} >Thursday, December 30</Text>
+        </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(255, 204, 2)'//rgb(5, 121, 255)'
+  },
+  timeText: {
+    fontSize: 75,
+    textAlign: "center",
+    margin: 10,
+  },
+  dateText: {
+    fontSize: 50,
+    textAlign: "center",
+    margin: 10,
+  },
+})
+
+export default App;
