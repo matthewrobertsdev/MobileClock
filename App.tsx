fda2a1958f29255dc79241bcdb8b2311e768a913
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2022-01-01 00:02:59 -0500

    Updates time and date every tenth of a second.

--- App.tsx
+++ App.tsx
@@ -27,6 +27,13 @@ import {
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const daysOfWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+  'September', 'October', 'November', 'December'];
+
+  const [timeString, setTimeString]=useState(getTimeStringFromDate(new Date()))
+  const [dateString, setDateString]=useState(getDateStringFromDate(new Date()))
+
   const statusBarStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     height: Platform.OS === 'ios' ? 50 : 0,
@@ -50,36 +57,101 @@ const App: () => Node = () => {
   const appState = useRef(AppState.currentState);
   const [appStateVisible, setAppStateVisible] = useState(appState.current);
 
+
   useEffect(() => {
+    console.log(appState)
     let startTimer = setInterval(() => {
-      console.log('tic')
-  }, 1000);
-  return () => clearTimeout(startTimer);
-  /*
-  const subscription = AppState.addEventListener("change", nextAppState => {
-    if (
-      appState.current.match(/inactive|background/) &&
-      nextAppState === "active"
-    ) {
-      console.log("App has gone to the foreground!");
-      clearInterval(startTimer)
-    }
-  });
+      const date=new Date();
+      console.log('tic');
+      setTimeString(getTimeStringFromDate(date));
+      setDateString(getDateStringFromDate(date));
+  }, 100);
+    /*
+    let subscription;
+    if (appState.current==='unknown') {
+      const startDate=new Date()
+    const millisecondsOffset=1000-startDate.getMilliseconds()
+    let timeOut = setTimeout(() => {
+      let firstDate=new Date()
+      setTimeString(getTimeStringFromDate(firstDate));
+      setDateString(getDateStringFromDate(firstDate));
+      startTimer = setInterval(() => {
+        const date=new Date();
+        console.log('tic');
+        setTimeString(getTimeStringFromDate(date));
+        setDateString(getDateStringFromDate(date));
+    }, 100);
+  }, millisecondsOffset);
+  } else {
+    subscription = AppState.addEventListener("change", nextAppState => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        console.log("App has come to the foreground!");
+    const startDate=new Date()
+    const millisecondsOffset=1000-startDate.getMilliseconds()
+    let timeOut = setTimeout(() => {
+      let firstDate=new Date()
+      setTimeString(getTimeStringFromDate(firstDate));
+      setDateString(getDateStringFromDate(firstDate));
+      startTimer = setInterval(() => {
+        const date=new Date();
+        console.log('tic');
+        setTimeString(getTimeStringFromDate(date));
+        setDateString(getDateStringFromDate(date));
+    }, 1000);
+  }, millisecondsOffset);
+      }
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+    })
+  }
   */
+  return () => {
+    if (startTimer!==null) {
+      clearTimeout(startTimer)
+  }
+  //if (subscription!==null) {
+    //subscription.remove();
+//}
+  };
 }, []);
 
   return (
     <View style={styles.container}>
       <View style={statusBarStyle}></View>
       <View style={styles.container}>
-        <Text style={styles.timeText} >12:00:00 PM</Text>
-        <Text style={styles.dateText} >Thursday, December 30</Text>
+        <Text style={styles.timeText} >{timeString}</Text>
+        <Text style={styles.dateText} >{dateString}</Text>
       </View>
       <View style={adStyle}></View>
       <View style={bottomStyle}>
       </View>
     </View>
   );
+  function getTimeStringFromDate(date: Date) {
+    let minutesString=''
+    let secondsString=''
+    const minutes=date.getMinutes()
+    const seconds=date.getSeconds()
+    if (minutes<10) {
+      minutesString=`0${minutes}`
+    } else {
+      minutesString=`${minutes}`
+    }
+    if (seconds<10) {
+      secondsString=`0${seconds}`
+    } else {
+      secondsString=`${seconds}`
+    }
+    return `${date.getHours()}:${minutesString}:${secondsString}`;
+  }
+  
+  function getDateStringFromDate(date: Date) {
+    return `${daysOfWeek[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()}`
+  }
+  
 };
 const styles = StyleSheet.create({
   container: {
