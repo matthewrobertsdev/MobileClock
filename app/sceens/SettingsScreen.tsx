106aa964ab85d8c53303ea3bc8a4d591983b5e0b
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2022-01-06 15:25:21 -0500

    Updated icons and now buttons aren't full width on Android.

--- app/sceens/SettingsScreen.tsx
+++ app/sceens/SettingsScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   View,
@@ -6,16 +6,17 @@ import {
   SafeAreaView
 } from 'react-native';
 
-import {useContext} from 'react';
+import { useContext } from 'react';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import SwitchWithText from '../components/SwitchWithText'
 import ButtonWithMargin from '../components/ButtonWithMargin'
 import { SettingsContext } from '../navigation/RootStackScreen';
+import { styles } from '../style/Style';
 
 
-function SettingsScreen({navigation}) {
+function SettingsScreen({ navigation }) {
   const [settings, setSettings] = useContext(SettingsContext);
   //save settings
   const saveShowSeconds = async (state) => {
@@ -59,35 +60,37 @@ function SettingsScreen({navigation}) {
     }
   }
   const [showsSeconds, setShowSeconds] = useState(settings.showsSeconds);
-  const toggleSeconds=()=>setShowSeconds(previousState=>{
+  const toggleSeconds = () => setShowSeconds(previousState => {
     saveShowSeconds(!previousState)
-    settings.showsSeconds=!previousState
+    settings.showsSeconds = !previousState
     setSettings(settings)
-    return !previousState})
+    return !previousState
+  })
   const [uses24HourTime, setUses24HourTime] = useState(settings.uses24HourTime);
-  const toggle24HourTime=()=>setUses24HourTime(previousState=>{
+  const toggle24HourTime = () => setUses24HourTime(previousState => {
     saveUse24HourTime(!previousState)
-    settings.uses24HourTime=!previousState
+    settings.uses24HourTime = !previousState
     setSettings(settings)
-    return !previousState})
+    return !previousState
+  })
   const [showsDate, setShowsDate] = useState(settings.showsDate);
-  const toggleShowsDate=()=>setShowsDate(previousState=>{
+  const toggleShowsDate = () => setShowsDate(previousState => {
     saveShowsDate(!previousState)
-    settings.showsDate=!previousState
+    settings.showsDate = !previousState
     setSettings(settings)
     return !previousState
   })
   const [showsDayOfWeek, setShowsDayOfWeek] = useState(settings.showsDayOfWeek);
-  const toggleShowsDayOfWeek=()=>setShowsDayOfWeek(previousState=>{
+  const toggleShowsDayOfWeek = () => setShowsDayOfWeek(previousState => {
     saveShowsDayOfWeek(!previousState)
-    settings.showsDayOfWeek=!previousState
+    settings.showsDayOfWeek = !previousState
     setSettings(settings)
     return !previousState
   })
   const [usesNumericalDate, setUsesNumericalDate] = useState(settings.usesNumericalDate);
-  const toggleUsesNumericalDate=()=>setUsesNumericalDate(previousState=>{
+  const toggleUsesNumericalDate = () => setUsesNumericalDate(previousState => {
     saveUsesNumericalDate(!previousState)
-    settings.usesNumericalDate=!previousState
+    settings.usesNumericalDate = !previousState
     setSettings(settings)
     return !previousState
   })
@@ -95,25 +98,27 @@ function SettingsScreen({navigation}) {
   }, []);
   //view
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <ScrollView>
-      <ButtonWithMargin text='Choose Color' onPress={()=>navigation.navigate('Colors')}/>
-      <View style={{flex: 1, alignItems: 'center'}}>
-        <SwitchWithText toggleSwitch={toggleSeconds} 
-        isEnabled={showsSeconds} text='Show Seconds'/>
-        <SwitchWithText toggleSwitch={toggle24HourTime} 
-        isEnabled={uses24HourTime} text='Use 24 Hour Time'/>
-        <SwitchWithText toggleSwitch={toggleShowsDate} 
-        isEnabled={showsDate} text='Show Date'/>
-        <SwitchWithText toggleSwitch={toggleShowsDayOfWeek} 
-        isEnabled={showsDayOfWeek} text='Show Day of Week'/>
-        <SwitchWithText toggleSwitch={toggleUsesNumericalDate} 
-        isEnabled={usesNumericalDate} disabled={!showsDate} text='Use Numerical Date'/>
-      </View>
-      <ButtonWithMargin text='Remove Ads'/>
-      <ButtonWithMargin text='Restore Purchases'/>
-      <ButtonWithMargin text='Help...' onPress={()=>navigation.navigate('Help')}/>
-      <ButtonWithMargin text='About Spiffy Clock' onPress={()=>navigation.navigate('About')}/>
+        <View style={styles.container}>
+          <ButtonWithMargin text='Choose Color' onPress={() => navigation.navigate('Colors')} />
+          <View style={{ flex: 1, alignItems: 'center' }}>
+            <SwitchWithText toggleSwitch={toggleSeconds}
+              isEnabled={showsSeconds} text='Show Seconds' />
+            <SwitchWithText toggleSwitch={toggle24HourTime}
+              isEnabled={uses24HourTime} text='Use 24 Hour Time' />
+            <SwitchWithText toggleSwitch={toggleShowsDate}
+              isEnabled={showsDate} text='Show Date' />
+            <SwitchWithText toggleSwitch={toggleShowsDayOfWeek}
+              isEnabled={showsDayOfWeek} text='Show Day of Week' />
+            <SwitchWithText toggleSwitch={toggleUsesNumericalDate}
+              isEnabled={usesNumericalDate} disabled={!showsDate} text='Use Numerical Date' />
+          </View>
+          <ButtonWithMargin text='Remove Ads' />
+          <ButtonWithMargin text='Restore Purchases' />
+          <ButtonWithMargin text='Help...' onPress={() => navigation.navigate('Help')} />
+          <ButtonWithMargin text='About Spiffy Clock' onPress={() => navigation.navigate('About')} />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
