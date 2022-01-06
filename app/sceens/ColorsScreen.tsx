ce102dae24a3bd74b16366375d214d64a03423a9
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2022-01-06 18:58:41 -0500

    Added colors and non-functioning UI for choosing colors.

--- app/sceens/ColorsScreen.tsx
+++ app/sceens/ColorsScreen.tsx
@@ -6,40 +6,74 @@
  * @flow strict-local
  */
 
- import React, { useContext, useState } from 'react';
- import {
-   SafeAreaView,
-   StatusBar,
-   useColorScheme,
-   View,
-   Text
- } from 'react-native';
+import React, { useContext, useState } from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+  View,
+  Text,
+  ScrollView
+} from 'react-native';
 
- 
- import { styles } from '../style/Style'
- 
- //holds settings
- import { SettingsContext } from '../navigation/RootStackScreen';
- 
- function ColorsScreen() {
-   const [settings, setSettings] = useContext(SettingsContext);
-   const [color, setColor] = useState('blue');
 
-   const isDarkMode = useColorScheme() === 'dark';
-    return (
-       <SafeAreaView style={{flex: 1}}>
-         <StatusBar />
-         <View style={styles.container}>
-           {/* Color preview background and text */}
-          <View style={styles.colorPreview}>
-            <Text style={styles.colorPreviewText}>
-              Color Preview
-            </Text>
+import { styles } from '../style/Style'
+
+//holds settings
+import { SettingsContext } from '../navigation/RootStackScreen';
+import { colorNames, darkColors, lightColors, lightDarkBackground } from '../style/Colors';
+import ButtonWithMargin from '../components/ButtonWithMargin';
+import SwitchWthText from '../components/SwitchWithText';
+function ColorsScreen() {
+  console.log(lightColors['Red'])
+  const [settings, setSettings] = useContext(SettingsContext);
+  const [color, setColor] = useState('blue');
+
+  const isDarkMode = useColorScheme() === 'dark';
+  const ColorCell = (props) => (
+    <View style={{
+      width: 300, height: 70,
+      backgroundColor: isDarkMode ? lightDarkBackground : 'white',
+      flexDirection: 'row', alignItems: 'center', margin: 10, borderRadius: 20
+    }}>
+      <View style={{
+        backgroundColor:
+          props.color,
+        width: 50, height: 50, borderRadius: 20,
+        margin: 10
+      }} />
+      <Text style={{ fontSize: 20, margin: 10, color: isDarkMode ? 'white' : 'black' }}>
+        {props.colorName}
+      </Text>
+    </View>
+  );
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar />
+      <View style={styles.container}>
+        {/* Color preview background and text */}
+        <View style={styles.colorPreview}>
+          <Text style={styles.colorPreviewText}>
+            Color Preview
+          </Text>
+        </View>
+        <ScrollView>
+          <View style={{ alignItems: 'center' }}>
+            <ButtonWithMargin text='Use Color for Background' />
+            <ButtonWithMargin text='Use Color for Foreground' />
+            <SwitchWthText text='Use Night Mode in Dark Mode' />
+            {colorNames.map(colorName => <ColorCell colorName={colorName} 
+            color={isDarkMode ? darkColors[colorName] : lightColors[colorName]}
+            key={colorName} />)}
+            <ButtonWithMargin text='Choose Custom Color...' />
+            <ColorCell colorName={'Custom Color'} 
+            color='gray'
+            key='Custom Color' />
           </View>
-         </View>
-       </SafeAreaView>
-     )
- };
- 
- export default ColorsScreen;
- 
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  )
+};
+
+export default ColorsScreen;
---
+++ app/style/Colors.tsx
@@ -0,0 +1,29 @@
+export const lightColors={
+  Red: 'rgb(255,59,48)',
+  Orange: 'rgb(255,149,2)',
+  Yellow: 'rgb(255,204,2)',
+  Green: 'rgb(41,205,64)',
+  Blue: 'rgb(3,122,255)',
+  Indigo: 'rgb(88,86,215)',
+  Purple: 'rgb(175,82,222)',
+  Pink: 'rgb(255,45,85)',
+  Brown: 'rgb(209,103,74)',
+}
+
+export const darkColors={
+  Red: 'rgb(171, 0, 27)',
+  Orange: 'rgb(179, 63, 0)',
+  Yellow: 'rgb(189, 134, 9))',
+  Green: 'rgb(0, 106, 0)',
+  Blue: 'rgb(2, 76, 182)',
+  Indigo: 'rgb(59,59,152)',
+  Purple: 'rgb(96, 31, 159)',
+  Pink: 'rgb(167, 40, 103)',
+  Brown: 'rgb(130,64,46)',
+}
+
+export const colorNames=['Red', 'Orange', 'Yellow', 'Green', 'Blue', 
+'Indigo', 'Purple', 'Pink', 'Brown']
+
+export const lightDarkBackground='rgb(28,28,30)'
+export const darkLightBackground='rgb(242,242,247)'
